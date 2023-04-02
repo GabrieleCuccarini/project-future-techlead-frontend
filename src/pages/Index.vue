ca256498eaccb0989d4f2ed49771c01e3bc654a0
GCucca <gabriele.cuccarini@gmail.com>
2023-04-02 12:40:38 +0200

    Added pagination + small graphic changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,11 +2,26 @@
 import axios from "axios";
 
 export default {
-  name:'Index', 
+  name: 'Index',
   data() {
     return {
-    backendUrl : 'http://localhost:8000',
-    perfumes: [],
+      backendUrl: 'http://localhost:8000',
+      perfumes: [],
+      currentPage: 1,
+      itemsPerPage: 15
+    }
+  },
+  computed: {
+    // Calcola il numero di pagine necessarie per visualizzare tutti i risultati
+    pageCount() {
+      return Math.ceil(this.perfumes.length / this.itemsPerPage);
+    },
+    // Ottieni l'array di profumi da visualizzare sulla pagina corrente
+    paginatedPerfumes() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      console.log('funzione paginated perfumes', startIndex, endIndex);
+      return this.perfumes.slice(startIndex, endIndex);
     }
   },
   methods: {
@@ -15,23 +30,47 @@ export default {
         .then((resp) => {
           this.perfumes = resp.data;
           console.log(this.perfumes);
-        });
+        })
+    },
+    nextPage() {
+      if (this.currentPage < this.pageCount) {
+        this.currentPage += 1;
+      }
+    },
+    // Vai alla pagina precedente
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage -= 1;
+      }
+    },    
+    goToPage(pageNumber) {
+      this.currentPage = pageNumber;
     }
   },
-  mounted(){
+  mounted() {
     this.fetchPerfumes();
   }
 }
 </script>
 
 <template>
+
+  <div class="pagination m-4">
+    <button @click="previousPage" :disabled="currentPage === 1"> <i class="fa-solid fa-arrow-left"></i> </button>
+      <!-- Loop attraverso i numeri di pagina e crea un pulsante per ognuno -->
+      <div v-for="pageNumber in pageCount" :key="pageNumber">
+        <button @click="goToPage(pageNumber)" :class="{ active: pageNumber === currentPage }">{{ pageNumber }}</button>
+      </div>
+    <button @click="nextPage" :disabled="currentPage === pageCount"> <i class="fa-solid fa-arrow-right"></i> </button>
+  </div>
   <div>
     <div class="row justify-content-evenly m-4">
-      <div class="col-xl-4 col-md-6 col-sm-9" v-for="perfume in perfumes" :key="perfume.id">
+      <div class="col-xl-4 col-md-6 col-sm-9" v-for="perfume in paginatedPerfumes" :key="perfume.id">
         <div class="card mb-3" style="max-width: 540px;">
           <div class="row g-0">
             <div class="col-md-6" v-if="perfume.cover_img">
-              <img :src="this.backendUrl + '/storage/' + perfume.cover_img" class="img-fluid asp-ratio rounded-start" alt="...">
+              <img :src="this.backendUrl + '/storage/' + perfume.cover_img" class="img-fluid asp-ratio rounded-start"
+                alt="...">
             </div>
             <div class="col-md-6">
               <div class="card-body">
@@ -46,6 +85,7 @@ export default {
       </div>
     </div>
   </div>
+
 </template>
 
 <style scoped lang="scss">
